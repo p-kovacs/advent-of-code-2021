34394c90f9a99a7c4f985465fee5e99d755dabf9
Péter Kovács <kovacspeter84@gmail.com>
2021-10-17 22:37:17 +0200

    Add skeleton classes and test for year 2021

---
+++ src/main/java/pkovacs/aoc/y2021/Day22.java
@@ -0,0 +1,15 @@
+package pkovacs.aoc.y2021;
+
+import pkovacs.aoc.AocUtils;
+import pkovacs.util.InputUtils;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        var lines = InputUtils.readLines(AocUtils.getInputPath());
+
+        System.out.println("Part 1: " + 0);
+        System.out.println("Part 2: " + 0);
+    }
+
+}
