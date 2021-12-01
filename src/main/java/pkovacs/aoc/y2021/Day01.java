6afeca997c75f05fcdc5546bac29803db6fd92fe
Péter Kovács <kovacspeter84@gmail.com>
2021-12-01 06:31:14 +0100

    Add solution for Day 1

--- src/main/java/pkovacs/aoc/y2021/Day01.java
+++ src/main/java/pkovacs/aoc/y2021/Day01.java
@@ -1,15 +1,24 @@
 package pkovacs.aoc.y2021;
 
+import java.util.stream.IntStream;
+
 import pkovacs.aoc.AocUtils;
 import pkovacs.util.InputUtils;
 
 public class Day01 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines(AocUtils.getInputPath());
+        var depths = InputUtils.readInts(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + countIncreases(depths, 1));
+        System.out.println("Part 2: " + countIncreases(depths, 3));
+    }
+
+    // Note: (x + y + z < y + z + w) is equivalent to (x < w)
+    private static long countIncreases(int[] values, int window) {
+        return IntStream.range(0, values.length - window)
+                .filter(i -> values[i] < values[i + window])
+                .count();
     }
 
 }
