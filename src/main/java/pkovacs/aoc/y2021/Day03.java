25d7174ee61edc16e18e62055c3d8d14c1189cdf
Péter Kovács <kovacspeter84@gmail.com>
2021-12-03 07:12:17 +0100

    Add solution for Day 3

--- src/main/java/pkovacs/aoc/y2021/Day03.java
+++ src/main/java/pkovacs/aoc/y2021/Day03.java
@@ -1,15 +1,49 @@
 package pkovacs.aoc.y2021;
 
+import java.util.List;
+
 import pkovacs.aoc.AocUtils;
 import pkovacs.util.InputUtils;
 
 public class Day03 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines(AocUtils.getInputPath());
+        var input = InputUtils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        long part1 = calculateRate(input, true) * calculateRate(input, false);
+        long part2 = calculateRating(input, true) * calculateRating(input, false);
+
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
+    }
+
+    private static int getPreferredBit(List<String> list, int index, boolean mostCommon) {
+        long count = list.stream().filter(v -> v.charAt(index) == '1').count();
+        int mostCommonBit = count >= list.size() - count ? 1 : 0;
+        return mostCommon ? mostCommonBit : 1 - mostCommonBit;
+    }
+
+    private static List<String> filter(List<String> list, int index, char ch) {
+        return list.stream().filter(s -> s.charAt(index) == ch).toList();
+    }
+
+    private static long calculateRate(List<String> list, boolean mostCommon) {
+        int bitCount = list.get(0).length();
+        long rate = 0;
+        for (int i = 0; i < bitCount; i++) {
+            rate = (rate << 1) | getPreferredBit(list, i, mostCommon);
+        }
+        return rate;
+    }
+
+    private static long calculateRating(List<String> input, boolean mostCommon) {
+        int bitCount = input.get(0).length();
+        List<String> list = input;
+        for (int i = 0; i < bitCount && list.size() > 1; i++) {
+            char bit = getPreferredBit(list, i, mostCommon) == 1 ? '1' : '0';
+            list = filter(list, i, bit);
+        }
+        return Long.parseLong(list.get(0), 2);
     }
 
 }
--- src/test/java/pkovacs/aoc/y2021/AocSolutionsTest.java
+++ src/test/java/pkovacs/aoc/y2021/AocSolutionsTest.java
@@ -21,7 +21,7 @@ public class AocSolutionsTest {
         return Stream.of(
                 new Arguments("Day01", Day01::main, "1301", "1346"),
                 new Arguments("Day02", Day02::main, "1693300", "1857958050"),
-                new Arguments("Day03", Day03::main, "0", "0"),
+                new Arguments("Day03", Day03::main, "2003336", "1877139"),
                 new Arguments("Day04", Day04::main, "0", "0"),
                 new Arguments("Day05", Day05::main, "0", "0"),
                 new Arguments("Day06", Day06::main, "0", "0"),
