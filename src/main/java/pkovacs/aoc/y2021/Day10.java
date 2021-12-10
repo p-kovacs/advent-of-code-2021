16a86ef6830602b2037f896248c2cf7fa9fcb49c
Péter Kovács <kovacspeter84@gmail.com>
2021-12-10 06:34:55 +0100

    Add solution for Day 10

--- src/main/java/pkovacs/aoc/y2021/Day10.java
+++ src/main/java/pkovacs/aoc/y2021/Day10.java
@@ -1,5 +1,9 @@
 package pkovacs.aoc.y2021;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import pkovacs.aoc.AocUtils;
 import pkovacs.util.InputUtils;
 
@@ -8,8 +12,66 @@ public class Day10 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        long invalidScore = 0;
+        var validScores = new ArrayList<Long>();
+        for (var line : lines) {
+            var stack = new ArrayDeque<Character>();
+            boolean valid = true;
+            for (int i = 0; i < line.length() && valid; i++) {
+                char ch = line.charAt(i);
+                switch (ch) {
+                    case '(', '[', '{', '<' -> stack.push(opposite(ch));
+                    case ')', ']', '}', '>' -> {
+                        if (stack.isEmpty() || !stack.pop().equals(ch)) {
+                            invalidScore += invalidScore(ch);
+                            valid = false;
+                        }
+                    }
+                }
+            }
+
+            if (valid) {
+                long score = 0;
+                while (!stack.isEmpty()) {
+                    score = score * 5 + validScore(stack.pop());
+                }
+                validScores.add(score);
+            }
+        }
+        Collections.sort(validScores);
+
+        System.out.println("Part 1: " + invalidScore);
+        System.out.println("Part 2: " + validScores.get(validScores.size() / 2));
+    }
+
+    private static Character opposite(char ch) {
+        return switch (ch) {
+            case '(' -> ')';
+            case '[' -> ']';
+            case '{' -> '}';
+            case '<' -> '>';
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    private static int validScore(char ch) {
+        return switch (ch) {
+            case ')' -> 1;
+            case ']' -> 2;
+            case '}' -> 3;
+            case '>' -> 4;
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    private static int invalidScore(char ch) {
+        return switch (ch) {
+            case ')' -> 3;
+            case ']' -> 57;
+            case '}' -> 1197;
+            case '>' -> 25137;
+            default -> throw new IllegalArgumentException();
+        };
     }
 
 }
