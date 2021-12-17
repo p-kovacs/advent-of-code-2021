481b467c645abdc568d9a238465f7bc9b793a423
Péter Kovács <kovacspeter84@gmail.com>
2021-12-17 17:06:45 +0100

    Remove unused imports

--- src/main/java/pkovacs/aoc/y2021/Day08.java
+++ src/main/java/pkovacs/aoc/y2021/Day08.java
@@ -2,7 +2,6 @@ package pkovacs.aoc.y2021;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
